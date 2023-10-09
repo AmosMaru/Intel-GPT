c565fedbb40d63f49191f2fefb4a7bb61773b470
jordanj0731 <jordanj0731@gmail.com>
2023-10-09 13:46:01 +0300

    flaskapp

--- 4.prototype-pandasai-openinterpreter/flaskapp.py
+++ 4.prototype-pandasai-openinterpreter/flaskapp.py
@@ -8,28 +8,33 @@ app.secret_key = 'your_secret_key'  # Change this to a strong secret key
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            filename = os.path.join('uploads', file.filename)
-            file.save(filename)
-            session['uploaded_file'] = filename
-            return redirect(url_for('chat'))
+    try:
+        if request.method == 'POST':
+            file = request.files['file']
+            if file:
+                filename = os.path.join('uploads', file.filename)
+                file.save(filename)
+                session['uploaded_file'] = filename
+                return redirect(url_for('chat'))
+    except Exception as e:
+        return str(e)  # You might want to return a user-friendly error message here
     return render_template('index.html')
 
-    
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
-    uploaded_file = session.get('uploaded_file')
-    if not uploaded_file:
-        return redirect(url_for('index'))
+    try:
+        uploaded_file = session.get('uploaded_file')
+        if not uploaded_file:
+            return redirect(url_for('index'))
 
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        df = pd.read_csv(uploaded_file)
-        result, img_str = chat_with_csv(df, prompt)  # Get both the result and the base64 string
-        return render_template('chat.html', result=result, img_data=img_str)
+        if request.method == 'POST':
+            prompt = request.form['prompt']
+            df = pd.read_csv(uploaded_file)
+            result, img_data = chat_with_csv(df, prompt)  # Get both the result and the base64 image
+            return render_template('chat.html', result=result, img_data=img_data)  # Pass img_data to the template
 
+    except Exception as e:
+        return str(e)  # You might want to return a user-friendly error message here
     return render_template('chat.html', result=None)
 
 if __name__ == '__main__':
