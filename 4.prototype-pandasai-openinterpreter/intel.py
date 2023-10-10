a3af83e9512cd70871b2ef38d7a46880a46642a5
jordanj0731 <jordanj0731@gmail.com>
2023-10-10 14:09:05 +0300

    changes on flaskapp

--- 4.prototype-pandasai-openinterpreter/intel.py
+++ 4.prototype-pandasai-openinterpreter/intel.py
@@ -1,5 +1,3 @@
-import matplotlib
-matplotlib.use('Agg')  # Add this line at the beginning
 from matplotlib import pyplot as plt
 import pandas as pd
 import interpreter
@@ -11,31 +9,34 @@ from dotenv import load_dotenv
 from pandasai import PandasAI
 import base64
 
-
 load_dotenv()
 
-openai_api_key = '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'
-image_folder_path = 'images'
-image_files = [os.path.join(image_folder_path, file) for file in os.listdir(image_folder_path) if file.endswith(('.jpg', '.png', '.jpeg'))]
-
+openai_api_key = os.getenv("OPENAI_API_KEY")
 interpreter.auto_run = True
 interpreter.api_key = openai_api_key
 
-def chat_with_csv(df,prompt):
+def chat_with_csv(df, prompt):
     llm = OpenAI(api_token=openai_api_key)
     pandas_ai = PandasAI(llm)
     result = pandas_ai.run(df, prompt=prompt)
 
-    # Add your plot creation code here
-    fig, ax = plt.subplots()
-    df.plot(ax=ax)
+    img_data = None  # Initialize img_data as None
 
-    # Save it to a BytesIO object
-    buf = io.BytesIO()
-    plt.savefig(buf, format='png')
-    buf.seek(0)
+    # Check if the result is not None and requires a plot
+    if result and 'plot' in result:
+        # Create and save the plot
+        fig, ax = plt.subplots()
+        df.plot(ax=ax)
 
-    # Convert the BytesIO object to a base64 string
-    img_str = base64.b64encode(buf.getvalue()).decode()
+        # Save it to a BytesIO object
+        buf = io.BytesIO()
+        plt.savefig(buf, format='png')
+        buf.seek(0)
 
-    return result, img_str  # Return both the result and the base64 string
+        # Convert the BytesIO object to a base64 string
+        img_data = base64.b64encode(buf.getvalue()).decode()
+
+    print(f"Result: {result}")  # Print the value of result
+    print(f"Image Data Length: {len(img_data)}")  # Print the length of img_data
+
+    return result, img_data  # Return both the result and the base64 image data
