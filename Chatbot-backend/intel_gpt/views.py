1496d30bc0fb80be79ec10f24d4e9c188670e5e1
Amos Kibet <amosmaru10@gmail.com>
2023-10-21 17:32:48 +0300

    update

--- Chatbot-backend/intel_gpt/views.py
+++ Chatbot-backend/intel_gpt/views.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from dotenv import load_dotenv
 load_dotenv()
-openai_api_key = ''
+openai_api_key = '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'
 interpreter.auto_run = True
 interpreter.api_key = openai_api_key
 
@@ -18,7 +18,7 @@ def home(request):
     documents = Document.objects.all()
     form = DocumentForm()
     chat_history = []  # This will store the chat history
-   
+
     if request.method == 'POST':
         if request.FILES:
             form = DocumentForm(request.POST, request.FILES)
@@ -29,16 +29,24 @@ def home(request):
             user_message = request.POST.get("name")
             chat_history.append({"user": True, "message": user_message})
 
+            # Open the DataFrame here
+            last_document = documents.last() if documents else None
+            df = None
+            if last_document:
+                with last_document.document.open() as f:
+                    df = pd.read_csv(f)
+
             # Process user message and get response
             # buffer = io.StringIO()
             # sys.stdout = buffer
-            # interpreter.chat(f"{user_message} use this dataset as a reference") 
+            # interpreter.chat(f"{user_message} use this dataset as a reference{df} and if there is plots to be graphed create and save the file in imeges folder in json formart") 
             # sys.stdout = sys.__stdout__
             # results = buffer.getvalue()
 
-            results = 'Niko poa sana.Tony How are you?'
+            results = df.head()
 
             chat_history.append({"user": False, "message": results})
+
 
     # Get the last uploaded document
     last_document = documents.last() if documents else None
