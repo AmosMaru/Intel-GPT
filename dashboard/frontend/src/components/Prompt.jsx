1c8cd86bb7915041ec6da15155c9558f4cf560e8
Amos Kibet <amosmaru10@gmail.com>
2023-10-28 11:17:25 +0300

    update

---
+++ dashboard/frontend/src/components/Prompt.jsx
@@ -0,0 +1,87 @@
+import { useState, useContext, useEffect } from "react"
+import { Context } from "../ContextProvider"
+import {baseURL} from "../data.json"
+import Upload from "./Upload"
+
+function Response({text}){
+    return(
+        <div className="my-2">{text}</div>
+    )
+}
+function Query({text}){
+    return(
+        <div className="float-right">{text}</div>
+    )
+}
+
+export default function Prompt(){
+    let { Chats } = useContext(Context);
+    let [chats, setChats] = Chats;
+    let [query, setQuery] = useState("")
+    let [upload, setUpload] = useState(false)
+
+    useEffect(()=>{
+        fetch(`${baseURL}/getChats?id=12345`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setChats(data.response)
+        })
+        .catch((err) => console.log("server error"));
+    },[])
+    useEffect(() => {
+        console.log("Clicked")
+        if (upload) {
+          document.getElementById("forms").style.visibility = "visible";
+        } else {
+          document.getElementById("forms").style.visibility = "hidden";
+        }
+      }, [upload]);
+
+    let submit=(e)=>{
+        e.preventDefault()
+        console.log(query)
+        fetch(`${baseURL}/query`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              query: query,
+            }),
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setChats([...chats,{query:query,response:data.response}])
+        })
+        .catch((err) => {
+            console.log("server error");
+            setChats([...chats,{query:query,response:"Sorry, Server error"}])
+        });
+    }
+    return(
+    <div class="p-4 rounded-lg border-gray-700 mt-14 lg:mt-16 lg:overflow-y-none text-white">
+        <div className="min-h-[90%]">
+            {
+                chats.map((chat)=>{
+                    return(
+                        <>
+                        <Query text={chat.query}/>
+                        <Response text={chat.response}/>
+                        </>
+                    )
+                })
+            }
+        </div>
+        <div className="flex justify-center">
+            <button className="block w-6" onClick={e=>{setUpload(true)}}><img className="w-full" src="/upload.svg" alt="" /></button>
+            <input className="text-black" type="text" placeholder="Enter query" value={query} onChange={e=>setQuery(e.target.value)} onKeyUp={e=>{if(e.key === "Enter")submit(e)}} />
+            <button className="block w-6" onClick={e=>submit(e)}><img className="block w-full" src="/send.svg" alt="" /></button>
+        </div>
+        <div id="forms" className="bg-black bg-opacity-40 h-screen w-screen absolute top-0 left-0 flex justify-center items-center">
+            <Upload control={setUpload} />
+        </div>
+    </div>
+    )
+}
